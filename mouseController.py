2b51a0b537534aad100c9ae9fb4cd6657d3a15e4
mcmuffindk <24530002+mcmuffindk@users.noreply.github.com>
2022-09-16 13:53:12 +0200

    Add comments

--- mouseController.py
+++ mouseController.py
@@ -7,21 +7,21 @@ screen_y = si.get_monitors()[0].height
 common_x = [0, 0, 0] # List of x coordinates
 common_y = [0, 0, 0] # List of y coordinates
 
-def getCommon(x, y):
-    common_x.append(x)
-    common_y.append(y)
-    common_x.pop(0)
-    common_y.pop(0)
-    return (sum(common_x) / len(common_x), sum(common_y) / len(common_y))
+def getCommon(x, y): # Get the common x and y coordinates
+    common_x.append(x) # Add the x coordinate to the list
+    common_y.append(y) # Add the y coordinate to the list
+    common_x.pop(0) # Remove the first element of the x list
+    common_y.pop(0) # Remove the first element of the y list
+    return (sum(common_x) / len(common_x), sum(common_y) / len(common_y)) # Return the average of the x and y coordinates
 
-def getX(c):
+def getX(c): # Get the x coordinate
     if (screen_x * c) > screen_x:
         return screen_x
     if (screen_x * c) < 0:
         return 0
     return screen_x * c
 
-def getY(c):
+def getY(c): # Get the y coordinate
     if (screen_y * c) > screen_y:
         return screen_y
     if (screen_y * c) < 0:
@@ -32,5 +32,5 @@ def move(x, y):
     comX, comY = getCommon(x, y)
     mouse.move(getX(comX), getY(comY))
 
-def click():
+def click(): # Click the mouse
     mouse.click()
