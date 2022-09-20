88158db01995ba592664c0b0295fd19ee6694ec2
mcmuffindk <24530002+mcmuffindk@users.noreply.github.com>
2022-09-20 12:42:34 +0200

    Change min confidence

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ cap = cv2.VideoCapture(0) # 0 is the id of the built-in camera
 with mp_hands.Hands(
     max_num_hands=2, # the maximum number of hands we want to track
     model_complexity=1, # 0 is the lowest complexity, 1 is the highest complexity
-    min_detection_confidence=0.5, # 0 is the lowest confidence, 1 is the highest confidence
-    min_tracking_confidence=0.5) as hands: # 0 is the lowest confidence, 1 is the highest confidence
+    min_detection_confidence=0.9, # 0 is the lowest confidence, 1 is the highest confidence
+    min_tracking_confidence=0.9) as hands: # 0 is the lowest confidence, 1 is the highest confidence
 
     # while webcam is open
     while cap.isOpened():
