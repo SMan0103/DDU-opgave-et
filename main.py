e952a3c88e73b29cffe9d5f436cb5c43089d4ad6
Tobias Christensen <Tobias@damsboe.dk>
2022-09-24 17:20:58 +0200

    Made mouse work good not bad
    Added increase to sensitivity to the mouse and increase precision of the clicking function and added delay on the clicks

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import mouseController as mc
+import time as t
 
 # get solutions
 mp_drawing = mp.solutions.drawing_utils
@@ -10,6 +11,14 @@ mp_hands = mp.solutions.hands
 # get webcam
 cap = cv2.VideoCapture(0) # 0 is the id of the built-in camera
 
+
+
+previousTime = 0
+timer = 0
+time = 1000 # in ms
+
+disableClick = False
+
 with mp_hands.Hands(
     max_num_hands=1, # the maximum number of hands we want to track
     model_complexity=1, # 0 is the lowest complexity, 1 is the highest complexity
@@ -18,6 +27,15 @@ with mp_hands.Hands(
 
     # while webcam is open
     while cap.isOpened():
+        if (disableClick == True):
+            currentTime = t.time() * 1000 
+            deltaTime = currentTime - previousTime
+            previousTime = currentTime
+            timer += deltaTime
+            if (timer >= time):
+                timer = 0
+                disableClick = False
+
         success, image = cap.read()
         if not success: # Check if the webcam is open
             print("Ignoring empty frame")
@@ -30,7 +48,7 @@ with mp_hands.Hands(
         image = cv2.resize(image, (imgWidth, imgHeight))
 
         # flip image horizontally to fix inverted x-axis
-        image = cv2.flip(image, 1)
+#        image = cv2.flip(image, 1)
 
         # mark image as not writeable to improve performance
         image.flags.writeable = False
@@ -50,15 +68,18 @@ with mp_hands.Hands(
         # if hands are detected
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
-                # get thumb tip
+                # get thumb tip landmark
                 thumb_tip = results.multi_hand_landmarks[0].landmark[4]
-                # get index finger tip
+                # get index finger tip landmark
                 index_tip = results.multi_hand_landmarks[0].landmark[8]
 
+
                 # if thumb and index finger are close together
-                if (thumb_tip.y - index_tip.y) <= 0.07:
+                if (thumb_tip.y - index_tip.y) <= 0.07 and (thumb_tip.x - index_tip.x) <= 0.07:
                     # click
-                    mc.click()
+                    if (disableClick == False):
+                        mc.click()
+                        disableClick = True
 
                 # move mouse to thumb tip
                 mc.move(thumb_tip.x, thumb_tip.y)
--- mouseController.py
+++ mouseController.py
@@ -1,8 +1,8 @@
 import mouse
 import screeninfo as si
 
-screen_x = si.get_monitors()[0].width
-screen_y = si.get_monitors()[0].height
+screen_x = (si.get_monitors()[0].width)
+screen_y = (si.get_monitors()[0].height)
 
 common_x = [0, 0, 0] # List of x coordinates
 common_y = [0, 0, 0] # List of y coordinates
@@ -15,17 +15,15 @@ def getCommon(x, y): # Get the common x and y coordinates
     return (sum(common_x) / len(common_x), sum(common_y) / len(common_y)) # Return the average of the x and y coordinates
 
 def getX(c): # Get the x coordinate
-    if (screen_x * c) > screen_x:
-        return screen_x
-    if (screen_x * c) < 0:
-        return 0
+    c = c*2-1
+    c = c*1.8
+    c = (c+1)/2
     return screen_x * c
 
 def getY(c): # Get the y coordinate
-    if (screen_y * c) > screen_y:
-        return screen_y
-    if (screen_y * c) < 0:
-        return 0
+    c = c*2-1
+    c = c*1.8
+    c = (c+1)/2
     return screen_y * c
 
 def move(x, y):
